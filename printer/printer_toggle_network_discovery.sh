36f7bb81f73b14bcc04ad353e4cb2e3104380e01
Marcus Magenta <mfm@magenta.dk>
2021-06-11 17:46:34 +0200

    Cleanup previous solution attempts

--- printer/printer_toggle_network_discovery.sh
+++ printer/printer_toggle_network_discovery.sh
@@ -7,7 +7,12 @@ set -ex
 # As a side effect all network printers previously found are removed 
 # and any you want, have to be added manually.
 # Log out or restart if changes don't take immediate effect.
-
+#
+# Attempted solutions that proved insufficient:
+# 1. Disable fx. BrowseProtocols in /etc/cups/cupsd.conf AND
+# /etc/cups/cups-browsed.conf
+# 2. Completely disable cups-browsed: systemctl mask cups-browsed
+#
 # Author: mfm@magenta.dk
 
 lower() {
@@ -16,33 +21,13 @@ lower() {
 
 ACTIVATE="$(lower "$1")"
 
-CUPS_CONFIG=/etc/cups/cups-browsed.conf
-
 if [ "$ACTIVATE" != 'fra' ] && [ "$ACTIVATE" != 'off' ]; then
   # Enable network printer discovery
   systemctl unmask avahi-daemon cups-browsed
   systemctl start avahi-daemon cups-browsed
 
-  #sed -i 's,BrowseProtocols none,# BrowseProtocols none,' $CUPS_CONFIG
-
-  # Alternate approach add these too
-  #sed -i 's,BrowseLocalProtocols none,# BrowseLocalProtocols none,' $CUPS_CONFIG
-  #sed -i 's,BrowseRemoteProtocols none,BrowseRemoteProtocols dnssd cups,' $CUPS_CONFIG
-
 else # Disable network printer discovery
   systemctl mask avahi-daemon cups-browsed
   # Mask vs. disable: https://askubuntu.com/a/816378/284161
   systemctl stop avahi-daemon cups-browsed
-
-  # Temporarily here to reset this setting to default from previous versions of
-  # this script, since it isn't needed anymore
-  #sed -i 's,BrowseProtocols none,# BrowseProtocols none,' $CUPS_CONFIG
-
-  #sed -i 's,# BrowseProtocols none,BrowseProtocols none,' $CUPS_CONFIG
-  # Alternate approach add these too
-  #sed -i 's,# BrowseLocalProtocols none,BrowseLocalProtocols none,' $CUPS_CONFIG
-  #sed -i 's,BrowseRemoteProtocols dnssd cups,BrowseRemoteProtocols none,' $CUPS_CONFIG
 fi
-
-# Restarting cups to reduce the chances of needing the restart:
-#systemctl reload-or-restart cups-browsed cups avahi-daemon
