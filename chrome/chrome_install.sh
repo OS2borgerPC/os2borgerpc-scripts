6a508c110bac469382f3e118a968b5d7e2e81e29
Marcus Funch Mogensen <mfm@magenta.dk>
2021-10-13 16:15:37 +0200

    Add disabling automatic updates as well, reformat

--- chrome/chrome_install.sh
+++ chrome/chrome_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /usr/bin/env sh
 
 # This script:
 # 1. Install google-chrome
@@ -6,6 +6,7 @@
 #    - prevents Google Chrome from asking if it should be default browser and about browser metrics
 #    - prevents the user logging in to the browser
 #    - disables the remember password prompt feature.
+#    - disable automatic updates including checks for them
 #
 # Author: Carsten Agger
 
@@ -13,10 +14,10 @@ set -x
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
-apt-get update
-apt-get install -y google-chrome-stable
+apt-get update --assume-yes
+apt-get install --assume-yes google-chrome-stable
 
-# Cleanup our previous policies if they're around
+# Cleanup our previous policies if they're around (except the homepage)
 OLD_POLICY_1="/etc/opt/chrome/policies/managed/os2borgerpc-default-hp.json"
 OLD_POLICY_2="/etc/opt/chrome/policies/managed/os2borgerpc-login.json"
 if [ -f "$OLD_POLICY_1" ]; then
@@ -30,16 +31,17 @@ fi
 POLICY="/etc/opt/chrome/policies/managed/os2borgerpc-defaults.json"
 
 if [ ! -d "$(dirname "$POLICY")" ]; then
-    mkdir -p "$(dirname "$POLICY")"
+    mkdir --parents "$(dirname "$POLICY")"
 fi
 
 # Additional info on the many policies that can be set:
 # https://support.google.com/chrome/a/answer/187202?hl=en
-cat > "$POLICY" <<END
-{
-    "DefaultBrowserSettingEnabled": true,
-    "MetricsReportingEnabled": false,
-    "BrowserSignin": 0,
-    "PasswordManagerEnabled": false
-}
+cat > "$POLICY" <<- END
+		{
+		    "DefaultBrowserSettingEnabled": true,
+		    "MetricsReportingEnabled": false,
+		    "BrowserSignin": 0,
+		    "PasswordManagerEnabled": false,
+		    "DeviceAutoUpdateDisabled": true
+		}
 END
