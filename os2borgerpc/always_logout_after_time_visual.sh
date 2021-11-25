afa1c83eff28beae91e2889aa7a361746bfa75f1
Marcus Funch Mogensen <mfm@magenta.dk>
2021-11-25 15:11:16 +0100

    Adjustments to the timer script

--- os2borgerpc/always_logout_after_time_visual.sh
+++ os2borgerpc/always_logout_after_time_visual.sh
@@ -1,13 +1,5 @@
 #! /usr/bin/env sh
 
-# TODO: It appears as if four timer processes are started?!
-#user        1635  0.0  0.0   2616   604 ?        S    13:52   0:00 /bin/sh /usr/share/os2borgerpc/bin/logout_timer_user.sh
-#root        1643  0.0  0.1  14852  4736 ?        S    13:52   0:00 sudo /usr/share/os2borgerpc/bin/logout_timer.sh
-#user        1650  0.0  0.0   2616  1048 ?        S    13:52   0:00 /bin/sh /usr/share/os2borgerpc/bin/logout_timer_user.sh
-#root        1701  0.0  0.0   2616  1580 ?        S    13:52   0:00 sh /usr/share/os2borgerpc/bin/logout_timer.sh
-
-
-# TODO: these timers should only run for "user"!
 # TODO: We need to run this program only AFTER login, so not graphical.target or whatever, if that
 # includes the login manager which also runs in X.
 # At the same time if switching from systemd enabling to systemd starting it from the PAM module,
@@ -15,16 +7,16 @@
 # We could add it to /etc/sudoers as no password and then run it from a .desktop file?
 # I would assume that would mean they can run it but they can't kill it?
 
-# TODO: This program needs to run as root or superuser so a user can't kill it,
+# This program needs to run as root or superuser so a user can't kill it,
 # but at the same time the timer program must run as the regular user to be able to write things to
 # screen?
 # It's fine if they kill zenity as long as they're then logged out automatically or the timer
 # continues in the background, or zenity is restarted. A systemd service?
-# Also get_os2borgerpc_config, to obtain the starting time, is not callable from user, we need root for that.
-# Also get_os2borgerpc_config is actually not relevant, because the total timer might be 24 minutes
-# but maybe they have 15 minutes left.
+#
 # So essentially when we check their user with PAM we need to calculate and save how much time they
 # have left, and then that's used as the starting point for the counter in this script
+
+set -x
 
 lower() {
     echo "$@" | tr '[:upper:]' '[:lower:]'
@@ -44,9 +36,10 @@ TIMER_LAUNCHER="/usr/share/os2borgerpc/bin/timer_launcher.sh"
 TIMERS_DESKTOP_FILE="/home/$SHADOW/.config/autostart/logout-timer_user.desktop"
 SUDOERS_CUSTOM=/etc/sudoers.d/os2borgerpc-cicero
 LOGOUT_TIMER_CONF=/usr/share/os2borgerpc/logout_timer.conf
+SESSION_CLEANUP_FILE=/usr/share/os2borgerpc/bin/user-cleanup.bash
 
 
-[ $# -lt 1 ] && printf "The script takes at least one argument: Whether to enable or disable the timer" && exit 1
+[ $# -lt 1 ] && printf "The script takes at least one argument: Whether to enable or disable the timer\n" && exit 1
 
 if [ "$ACTIVATE" != 'false' ] && [ "$ACTIVATE" != 'falsk' ] && \
    [ "$ACTIVATE" != 'no' ] && [ "$ACTIVATE" != 'nej' ]; then
@@ -62,9 +55,7 @@ if [ "$ACTIVATE" != 'false' ] && [ "$ACTIVATE" != 'falsk' ] && \
 
 		# Remember to NOT run this script as the regular user as otherwise they can just kill the process
 
-		. $LOGOUT_TIMER_CONF
-
-		COUNT=\$TIME_SECONDS
+		COUNT=\$1
 
 		until [ "\$COUNT" -eq "0" ]; do                              # Countdown loop.
 		    COUNT=\$((COUNT-1))                                      # Decrement seconds.
@@ -89,21 +80,20 @@ chmod 0440 $SUDOERS_CUSTOM
 	cat <<- EOF > $TIMER_USER_PROGRAM
     #! /usr/bin/env sh
 
+		# Credits: https://handybashscripts.blogspot.com/2012/01/simple-timer-with-progress-bar.html
+
+    set -x
+
     # We need job control to move the window but then suspend until zenity closes
     set -m
 
-		# Credits: https://handybashscripts.blogspot.com/2012/01/simple-timer-with-progress-bar.html
+    TIME_SECONDS=\$1
 
     TITLE="Logintid"
 
-    . $LOGOUT_TIMER_CONF
-
     GRACE_PERIOD_SECONDS=15
-    # Subtracting a little from this as we need this to run at least a bit before they're logged out
+    # Subtracting a little from this as we need this to run out at least a bit before they're logged out
     COUNT=\$((TIME_SECONDS - GRACE_PERIOD_SECONDS))
-
-		#ICON=/usr/share/icons/Yaru/48x48/apps/clock-app.png
-		ICON=clock-app
 
 		START=\$COUNT                                                # Set a starting point.
 
@@ -114,11 +104,13 @@ chmod 0440 $SUDOERS_CUSTOM
 		    echo \$PERCENT                                           # Output for progbar.
 		    #echo \$COUNT | xsel -i -p
 		    sleep 1
-		done | zenity --title "$TITLE" --progress --percentage=0 --text="" \
-		    --window-icon \$ICON --icon-name \$ICON --auto-close --no-cancel &               # Progbar/time left.
+		done | zenity --title "\$TITLE" --progress --percentage=0 --text="" \
+		    --auto-close --no-cancel &               # Progbar/time left.
 
     # xdotool would not work in Wayland
-    xdotool windowmove "$(xdotool search "$TITLE")" $X_POSITION $Y_POSITION
+    sleep 3
+    xdotool windowmove "\$(xdotool search --name "\$TITLE")" $X_POSITION $Y_POSITION
+    echo "xdotool windowmove "\$(xdotool search --name "\$TITLE")" $X_POSITION $Y_POSITION" > /home/user/wat.txt
     fg
 
 		#xsel -o -p
@@ -134,10 +126,12 @@ chmod 0440 $SUDOERS_CUSTOM
 
 		# Using bash because disown is undefined in POSIX sh
 
-		$TIMER_USER_PROGRAM &
+    # Fetch TIME_SECONDS, the number of seconds it should count down
+		. $LOGOUT_TIMER_CONF
+
+		$TIMER_USER_PROGRAM \$TIME_SECONDS &
 		disown
-		sudo $TIMER_LOGOUT_PROGRAM &
-		disown
+		sudo $TIMER_LOGOUT_PROGRAM \$TIME_SECONDS
 	EOF
 
   mkdir --parents /home/$SHADOW/.config/autostart
@@ -152,8 +146,22 @@ chmod 0440 $SUDOERS_CUSTOM
 		X-GNOME-Autostart-enabled=true
 	EOF
 
+  # Modify the cleanup run at logout to also kill remaining timers so they don't persist affecting
+  # the next login
+  if ! grep -q "Logintid" "$SESSION_CLEANUP_FILE"; then
+		cat <<- EOF >> $SESSION_CLEANUP_FILE
+			pkill -f Logintid
+			pkill -f $(basename $TIMER_LOGOUT_PROGRAM)
+			pkill -f $(basename $TIMER_LAUNCHER)
+		EOF
+  fi
+
   chmod +x $TIMER_LOGOUT_PROGRAM $TIMER_USER_PROGRAM $TIMER_LAUNCHER $TIMERS_DESKTOP_FILE
 
 else # Delete the timer
   rm $TIMER_LOGOUT_PROGRAM $TIMER_USER_PROGRAM $TIMER_LAUNCHER $TIMERS_DESKTOP_FILE
+  # Remove the cleanup of timer processes
+  sed --in-place "/pkill -f Logintid/d" $SESSION_CLEANUP_FILE
+  sed --in-place "/pkill -f $(basename $TIMER_LOGOUT_PROGRAM)/d" $SESSION_CLEANUP_FILE
+  sed --in-place "/pkill -f $(basename $TIMER_LAUNCHER)/d" $SESSION_CLEANUP_FILE
 fi
