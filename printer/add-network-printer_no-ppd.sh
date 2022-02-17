f9976c74dfc561d0bfb889130b3df87bd948ed11
Marcus Funch Mogensen <mfm@magenta.dk>
2022-02-17 09:00:45 +0100

    Consistency in regards to quotes between network printer scripts and princh scripts

--- printer/add-network-printer_no-ppd.sh
+++ printer/add-network-printer_no-ppd.sh
@@ -2,15 +2,12 @@
 
 set -e
 
-
 # Vars
-printer_name=$1
-printer_loc=$2
-printer_descr=$3
-
+printer_name="$1"
+printer_loc="$2"
+printer_descr="$3"
 
 printer_conn="ipp://"
-
 
 # Execute command with user defined vars
 lpadmin -p "$printer_name" -v "$printer_conn$printer_loc" -D "$printer_descr" -E -m everywhere
