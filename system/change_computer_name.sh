a450a42b8571d1721a115a68415cc0ef7bceff8e
Marcus Funch Mogensen <mfm@magenta.dk>
2022-02-18 13:40:49 +0100

    When renaming a computer, also rename it in the PC's configuration

--- system/change_computer_name.sh
+++ system/change_computer_name.sh
@@ -1,19 +1,31 @@
-#!/usr/bin/env bash
+#! /usr/bin/env sh
 
 # This script will change the name of the computer, it will not
-# change how it looks in the admin. There will be no validation that this parameter is a valid name.
+# change how it looks in the admin. There is no validation that this parameter is a valid name.
+# Requirements for a valid hostname: https://www.man7.org/linux/man-pages/man7/hostname.7.html
 
-if [ $# -ne 1 ]
-then
-    echo "This script takes exactly one argument."
+set -e
+
+NEW_COMPUTER_NAME=$1
+
+PREFS_FILE=/etc/hosts
+OLD_COMPUTER_NAME=$(hostname)
+
+if [ $# -ne 1 ]; then
+    printf '%s\n' "This script needs exactly one argument: The new name of the computer."
     exit 1
 fi
 
-NEW_NAME=$1
-PREFS_FILE=/etc/hosts
-OLD_NAME=$(hostname)
+# Update the name in /etc/hostname
+hostnamectl set-hostname "$NEW_COMPUTER_NAME"
 
-hostname "$NEW_NAME"
-# Replace old line with new
-sed -i "s/$OLD_NAME/$NEW_NAME/g" $PREFS_FILE
-echo "$NEW_NAME" > /etc/hostname
+# Also update the name in /etc/hosts
+sed -i "s/$OLD_COMPUTER_NAME/$NEW_COMPUTER_NAME/g" $PREFS_FILE
+
+# Also update the name in the computer's Configuration in OS2borgerPC locally
+set_os2borgerpc_config hostname "$NEW_COMPUTER_NAME"
+# ...and push that change to the adminsite, so it isn't overwritten locally when jobmanager runs
+os2borgerpc_push_config_keys hostname
+
+printf '%s\n' "Rename done!"
+printf '%s\n' "Now manually update the PC's name in the admin interface and it will have been renamed everywhere."
