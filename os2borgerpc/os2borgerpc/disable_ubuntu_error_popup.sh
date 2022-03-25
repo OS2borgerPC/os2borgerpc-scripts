58b071d3624c6412f193e37e0e90611d6dcbcfb8
Heini Leander Ovason <heini.ovason@gmail.com>
2022-03-25 15:06:09 +0100

    Script added

---
+++ os2borgerpc/os2borgerpc/disable_ubuntu_error_popup.sh
@@ -0,0 +1,46 @@
+#! /usr/bin/env sh
+
+set -x
+
+ACTIVATE=$1
+
+# Change these three to set a different policy to another value
+POLICY_PATH="com/ubuntu/update-notifier"
+POLICY="show-apport-crashes"
+POLICY_VALUE="false"
+
+POLICY_FILE="/etc/dconf/db/os2borgerpc.d/00-$POLICY"
+POLICY_LOCK="/etc/dconf/db/os2borgerpc.d/locks/00-$POLICY"
+
+
+if [ "$ACTIVATE" = 'True' ]; then
+
+	mkdir --parents "$(dirname $POLICY_FILE)" "$(dirname $POLICY_LOCK)"
+
+	# dconf does not, by default, require the use of a system database, so
+	# add one (called "os2borgerpc") to store our system-wide settings in
+	cat > "/etc/dconf/profile/user" <<-END
+		user-db:user
+		system-db:os2borgerpc
+	END
+
+	cat > "$POLICY_FILE" <<-END
+		[$POLICY_PATH]
+		$POLICY=$POLICY_VALUE
+	END
+	# "dconf update" will only act if the content of the keyfile folder has
+	# changed: individual files changing are of no consequence. Force an update
+	# by changing the folder's modification timestamp
+	touch "$(dirname "$POLICY_FILE")"
+
+	# Tell the system that the values of the dconf keys we've just set can no
+	# longer be overridden by the user
+	cat > "$POLICY_LOCK" <<-END
+		/$POLICY_PATH/$POLICY
+	END
+else
+	rm -f "$POLICY_FILE" "$POLICY_LOCK"
+fi
+
+# Incorporate all of the text files we've just created into the system's dconf databases
+dconf update
