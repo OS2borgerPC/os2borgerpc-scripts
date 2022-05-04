6c33d63f0bc5f6abf44f77903b8e522c5b82133c
Marcus Funch <mfm@magenta.dk>
2022-05-04 11:27:28 +0200

    Update all: Add fix-missing, use more readable long form arguments instead

--- common/system/update_all.sh
+++ common/system/update_all.sh
@@ -1,29 +1,19 @@
 #!/usr/bin/env bash
 
-#================================================================
-#% SYNOPSIS
-#+    update_all.sh
-#%
-#% DESCRIPTION
-#%    This script updates all apt repositories and then applies all available
-#%    upgrades, picking default values for all debconf questions. It takes no
-#%    parameters.
-#%
-#================================================================
-#- IMPLEMENTATION
-#-    version         update_all.sh (magenta.dk) 1.0.0
-#-    author          Alexander Faithfull
-#-    copyright       Copyright 2019, Magenta ApS
-#-    license         GNU General Public License
-#-    email           af@magenta.dk
-#-
-#================================================================
-#  HISTORY
-#     2019/07/16 : af : Script updated
+# SYNOPSIS
+#    update_all.sh
 #
-#================================================================
-# END_OF_HEADER
-#================================================================
+# DESCRIPTION
+#    This script updates all apt repositories and then applies all available
+#    upgrades, picking default values for all debconf questions. It takes no
+#    parameters.
+#
+# IMPLEMENTATION
+#    version         update_all.sh (magenta.dk) 1.0.0
+#    author          Alexander Faithfull
+#    copyright       Copyright 2019, Magenta ApS
+#    license         GNU General Public License
+#    email           af@magenta.dk
 
 set -e
 
@@ -38,9 +28,11 @@ EOF
 # Stop Debconf from doing anything
 export DEBIAN_FRONTEND=noninteractive
 
+# Update apt packages
+# Snap packages are updated automatically by default in Ubuntu
 apt-get update > /dev/null
-apt-get -yf install # Attempt to fix broken or interrupted installations
-apt-get -y upgrade
-apt-get -y dist-upgrade
-apt-get -y autoremove
-apt-get -y clean
+apt-get --assume-yes --fix-broken --fix-missing install # Attempt to fix broken or interrupted installations, and add missing packages
+apt-get --assume-yes upgrade
+apt-get --assume-yes dist-upgrade
+apt-get --assume-yes autoremove
+apt-get --assume-yes clean
