e91ce92ee42cafa0fde93d8e2b08de4873e3c152
Søren Howe Gersager <thesyre@pm.me>
2022-05-17 11:23:42 +0200

    update ping _event

--- common/security/ping_event.py
+++ common/security/ping_event.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
+from datetime import datetime
 
-from os2borgerpc.client.security import csv_writer
 
-# securityEventCode, Tec sum, Raw data
-csv_data = []
-# securityEventCode (security problem id)
-csv_data.append("%SECURITY_PROBLEM_UID%")
+def csv_writer(security_events):
+    """Write security events to security events file."""
+    with open("/etc/os2borgerpc/security/securityevent.csv", "at") as csvfile:
+        for timestamp, security_problem_uid, log_event, complete_log in security_events:
+            event_line = log_event.replace("\n", " ").replace("\r", "").replace(",", "")
+            csvfile.write(
+                f"{timestamp},{security_problem_uid},{event_line},{complete_log}\n"
+            )
 
-# Tec sum
-csv_data.append("Ping warning.")
 
-# Raw data
-csv_data.append("Nothing to worry about. Just a ping warning.")
+now = datetime.now()
+timestamp = datetime.strftime(now, "%Y%m%d%H%M")
+security_problem_uid_template_var = "%SECURITY_PROBLEM_UID%"
+log_event = "Ping warning."
+complete_log = "Nothing to worry about. Just a ping warning."
 
-csv_writer.write_data(csv_data)
+
+csv_writer([(timestamp, security_problem_uid_template_var, log_event, complete_log)])
