50c3b98bdb1ba70e500493b5494aa67aef750a96
Søren Howe Gersager <thesyre@pm.me>
2022-05-20 13:10:19 +0200

    add noqa for log line example

--- common/security/keyboard_event.py
+++ common/security/keyboard_event.py
@@ -83,7 +83,7 @@ security_problem_uid_template_var = "%SECURITY_PROBLEM_UID%"
 # Match keyboard events that are after 9.9999 seconds of boot up
 # (so we don't match upstart keyboard events).
 # Example:
-# May 20 11:29:33 kbh-nuc-hoejre kernel: [ 10.122061] input: Dell Dell USB Keyboard as /devices/pci0000:00/0000:00:14.0/usb1/1-3/1-3:1.0/0003:413C:2003.0003/input/input8
+# May 20 11:29:33 kbh-nuc-hoejre kernel: [ 10.122061] input: Dell Dell USB Keyboard as /devices/pci0000:00/0000:00:14.0/usb1/1-3/1-3:1.0/0003:413C:2003.0003/input/input8 # noqa: E501
 regexes = [r".*\[[ ]{0,3}[0-9]{2,}\..*\] input: .*Keyboard.*"]
 
 # Filter log_event_tuples based on regex matches and put them
