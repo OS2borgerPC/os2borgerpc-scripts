7a8c035c351ba062eb0ca7e7a344610b3fdd42ad
Marcus Funch <mfm@magenta.dk>
2022-06-03 16:28:28 +0200

    htpdate: Update to disable systemd-timesyncd, thinking it might interfere

--- common/system/set_time_htpdate.sh
+++ common/system/set_time_htpdate.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 
-# Synchronize time with htpdate.
+# One-off synchronize time with htpdate.
 # Used on instances where NTP ports or NTP synchronization in general are blocked.
 
 set -ex
@@ -13,6 +13,10 @@ date
 htpdate -s www.pool.ntp.org dk.pool.ntp.org www.wikipedia.org
 echo "time after updating"
 date
+
+# Disabling this service since it's under suspicion for interfering and syncing the time back to be incorrect
+# (if NTP is blocked by the firewall)
+systemctl disable systemd-timesyncd
 
 # Clean up and remove htpdate, as otherwise it leaves a service running which continually syncs time
 # via htpdate
