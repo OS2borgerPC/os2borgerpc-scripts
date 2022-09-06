b1d0af6d02c6d746e23bd3b760f21c5ffd5da72e
Andreas Poulsen <ap@magenta-aps.dk>
2022-09-06 13:04:53 +0200

    implemented feedback to lockdown script with pre-approval

--- os2borgerpc/os2borgerpc/lockdown_usb_unless_approved.sh
+++ os2borgerpc/os2borgerpc/lockdown_usb_unless_approved.sh
@@ -57,28 +57,33 @@ if [ "$UNLOCK" = "True" ]; then
 fi
 
 if [ "$ACTIVATE" = "True" ]; then
-    FILE=/usr/local/lib/os2borgerpc/usb-monitor
+    FILE=/etc/systemd/system/os2borgerpc-usb-monitor.service
     if [ -f "$FILE" ]; then
         systemctl disable --now os2borgerpc-usb-monitor.service
     fi
-    mkdir -p /usr/local/lib/os2borgerpc
+    mkdir -p /usr/share/os2borgerpc/lib
 
-    cat << EOF > /usr/local/lib/os2borgerpc/approve-devices
+    cat << EOF > /usr/share/os2borgerpc/lib/approve-devices.py
 #!/usr/bin/env python3
 
 import subprocess
 import re
+from os.path import exists
 
 APPROVED_LIST = "/etc/os2borgerpc/approved_usb_ids.txt"
 APPEND = $APPEND
 
 def get_approved_ids():
-    """Load the list of approved device ids."""
-    with open(APPROVED_LIST) as file:
-        approved_ids = file.readlines()
-    for i in range(len(approved_ids)):
-        approved_ids[i] = approved_ids[i][:-1]
-    return approved_ids
+    """Load the list of approved device ids. If no list
+    has been made, return an empty list."""
+    if not exists(APPROVED_LIST):
+        return []
+    else:
+        with open(APPROVED_LIST) as file:
+            approved_ids = file.readlines()
+        for i in range(len(approved_ids)):
+            approved_ids[i] = approved_ids[i][:-1]
+        return approved_ids
 
 def approve_devices(append=False):
     """Get the ids of all currently connected usb devices
@@ -87,35 +92,36 @@ def approve_devices(append=False):
     If append is False, the existing list is replaced
     with a list containing the current ids."""
     encoding = 'utf-8'
-    df = subprocess.check_output("lsusb")
+    lsusb_output = subprocess.check_output("lsusb")
     device_ids = []
-    for info in df.split(b'\n'):
+    for info in lsusb_output.split(b'\n'):
         if info:
             id = re.search(b"ID\s(\w+:\w+)", info).group(1)
             if id:
                 device_ids.append(id)
     device_ids = list(dict.fromkeys(device_ids))
+    device_ids = [str(device_id, encoding) for device_id in device_ids]
     if append:
         current_list = get_approved_ids()
-        with open(APPROVED_LIST, 'a') as file:
-            for device_id in device_ids:
-                if str(device_id, encoding) not in current_list:
-                    file.write(str(device_id, encoding))
-                    file.write("\n")
+        new_ids = [id for id in device_ids if id not in current_list]
+        if new_ids:
+            with open(APPROVED_LIST, 'a') as file:
+                file.write('\n'.join(new_ids))
+                file.write('\n')
     else:
         with open(APPROVED_LIST, 'w') as file:
-            for device_id in device_ids:
-                file.write(str(device_id, encoding))
-                file.write("\n")
+            file.write('\n'.join(device_ids))
+            file.write('\n')
 
 if __name__ == "__main__":
     # Pre-approve currently connected usb devices
     approve_devices(APPEND)
 EOF
-    python3 /usr/local/lib/os2borgerpc/approve-devices
-    rm -f /usr/local/lib/os2borgerpc/approve-devices
+    chmod 700 /usr/share/os2borgerpc/lib/approve-devices.py
+    /usr/share/os2borgerpc/lib/approve-devices.py
+    rm -f /usr/share/os2borgerpc/lib/approve-devices.py
 
-    cat << EOF > /usr/local/lib/os2borgerpc/usb-monitor
+    cat << EOF > /usr/share/os2borgerpc/lib/usb-monitor.py
 #!/usr/bin/env python3
 
 from os import mkfifo, unlink
@@ -137,9 +143,9 @@ def lockdown():
 def get_current_ids():
     """Get the ids of the currently connected usb devices."""
     encoding = 'utf-8'
-    df = subprocess.check_output("lsusb")
+    lsusb_output = subprocess.check_output("lsusb")
     device_ids = []
-    for info in df.split(b'\n'):
+    for info in lsusb_output.split(b'\n'):
         if info:
             id = re.search(b"ID\s(\w+:\w+)", info).group(1)
             if id:
@@ -166,9 +172,6 @@ def get_event_id(usb_event):
         return None
     else:
         id_path = path_info[1]
-    # path_list = re.findall("bind\s(.+)", usb_event, re.M)
-    # path_lengths = [len(path) for path in path_list]
-    # id_path = path_list[path_lengths.index(max(path_lengths))]
         id_path = id_path + "/modalias"
         if not exists(id_path):
             return None
@@ -223,7 +226,7 @@ def main():
 if __name__ == "__main__":
     main()
 EOF
-    chmod 700 /usr/local/lib/os2borgerpc/usb-monitor
+    chmod 700 /usr/share/os2borgerpc/lib/usb-monitor.py
 
     cat <<"END" > /etc/systemd/system/os2borgerpc-usb-monitor.service
 [Unit]
@@ -231,7 +234,7 @@ Description=OS2borgerPC USB monitoring service
 
 [Service]
 Type=simple
-ExecStart=/usr/local/lib/os2borgerpc/usb-monitor
+ExecStart=/usr/share/os2borgerpc/lib/usb-monitor.py
 # It's important that we stop the Python process, stuck in a blocking read,
 # with SIGINT rather than SIGTERM so that its finaliser has a chance to run
 KillSignal=SIGINT
@@ -241,7 +244,7 @@ WantedBy=display-manager.service
 END
     systemctl enable --now os2borgerpc-usb-monitor.service
 
-    cat <<"END" > /usr/local/lib/os2borgerpc/on-usb-event
+    cat <<"END" > /usr/share/os2borgerpc/lib/on-usb-event.sh
 #!/bin/sh
 
 if [ -p "/var/lib/os2borgerpc/usb-event" ]; then
@@ -251,16 +254,16 @@ if [ -p "/var/lib/os2borgerpc/usb-event" ]; then
             of=/var/lib/os2borgerpc/usb-event status=none
 fi
 END
-    chmod 700 /usr/local/lib/os2borgerpc/on-usb-event
+    chmod 700 /usr/share/os2borgerpc/lib/on-usb-event.sh
 
     cat <<"END" > /etc/udev/rules.d/99-os2borgerpc-usb-event.rules
-SUBSYSTEM=="usb", TEST=="/var/lib/os2borgerpc/usb-event", RUN{program}="/usr/local/lib/os2borgerpc/on-usb-event '%E{ACTION}' '$sys$devpath'"
+SUBSYSTEM=="usb", TEST=="/var/lib/os2borgerpc/usb-event", RUN{program}="/usr/share/os2borgerpc/lib/on-usb-event.sh '%E{ACTION}' '$sys$devpath'"
 END
 else
     systemctl disable --now os2borgerpc-usb-monitor.service
-    rm -f /usr/local/lib/os2borgerpc/on-usb-event \
+    rm -f /usr/share/os2borgerpc/lib/on-usb-event.sh \
             /etc/udev/rules.d/99-os2borgerpc-usb-event.rules \
-            /usr/local/lib/os2borgerpc/usb-monitor \
+            /usr/share/os2borgerpc/lib/usb-monitor.py \
             /etc/systemd/system/os2borgerpc-usb-monitor.service \
             /etc/os2borgerpc/approved_usb_ids.txt
 fi
