0057a927e4a6e282b58b1d9bc8f0ca6230d36c3e
Marcus Funch <mfm@magenta.dk>
2022-11-09 16:42:39 +0100

    Protect terminal: Build in reversal for the script, for easier testing

--- os2borgerpc/sikkerhed/protect_terminal.sh
+++ os2borgerpc/sikkerhed/protect_terminal.sh
@@ -2,26 +2,51 @@
 
 set -ex
 
-if [ !  -f "/usr/bin/gnome-terminal.real" ]
-then
-    dpkg-divert --rename --divert  /usr/bin/gnome-terminal.real --add /usr/bin/gnome-terminal
-    dpkg-statoverride --update --add superuser root 770 /usr/bin/gnome-terminal.real
+ACTIVATE=$1
+PROGRAM_OLD_PATH="/usr/bin/gnome-terminal"
+PROGRAM_NEW_PATH="$PROGRAM_OLD_PATH.real"
+
+# Restore access
+if [ "$ACTIVATE" = 'True' ]; then
+  # Making sure we're not removing the actual
+  # gnome-terminal if run with the wrong argument or multiple times
+  if grep --quiet 'zenity' "$PROGRAM_OLD_PATH"; then
+    # Remove the permissions override and manually reset permissions to defaults
+     # Suppress error to prevent set -e exiting in case the override no longer exists
+    dpkg-statoverride --remove "$PROGRAM_NEW_PATH" || true
+    chown root:root "$PROGRAM_OLD_PATH"
+    chmod 755 "$PROGRAM_NEW_PATH"
+    # Remove the shell script that prints the error message
+    rm "$PROGRAM_OLD_PATH"
+    # Remove location override and restore gnome-terminal.real back to gnome-terminal
+    dpkg-divert --remove "$PROGRAM_OLD_PATH"
+    # dpkg-divert can --rename it itself, but the problem with doing that is that in some images
+    # dpkg-divert is not used, it was simply moved/copied, so that won't restore it, leaving you
+    # with no gnome-control-center
+    mv "$PROGRAM_NEW_PATH" "$PROGRAM_OLD_PATH"
+  fi
+else # Deny access
+
+  if [ !  -f "$PROGRAM_NEW_PATH" ] # Don't divert and statoverride if they've already been done (idempotency)
+  then
+      dpkg-divert --rename --divert  "$PROGRAM_NEW_PATH" --add "$PROGRAM_OLD_PATH"
+      dpkg-statoverride --update --add superuser root 770 "$PROGRAM_OLD_PATH"
+  fi
+
+	cat <<- EOF > "$PROGRAM_OLD_PATH"
+		#!/bin/bash
+
+		USER=\$(id -un)
+
+		if [ \$USER == "user" ]; then
+		  zenity --info --text="Terminalen er ikke tilgængelig for publikum."
+		else
+		  "$PROGRAM_NEW_PATH"
+		fi
+	EOF
+
+  chmod +x "$PROGRAM_OLD_PATH"
+
+  # Also remove the gnome extension that can start gnome terminal
+  apt-get remove --assume-yes nautilus-extension-gnome-terminal
 fi
-
-
-cat << EOF > /usr/bin/gnome-terminal
-#!/bin/bash
-
-USER=\$(id -un)
-
-if [ \$USER == "user" ]; then
-  zenity --info --text="Terminalen er ikke tilgængelig for publikum."
-else
-  /usr/bin/gnome-terminal.real
-fi
-
-EOF
-
-chmod +x /usr/bin/gnome-terminal
-
-apt-get remove --assume-yes nautilus-extension-gnome-terminal
