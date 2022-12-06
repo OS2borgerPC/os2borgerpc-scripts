cdaa4d421d3f38217a386289dce93431edd667fd
heini.ovason <heini.ovason@protonmail.com>
2022-12-06 10:51:26 +0100

    Dynamically generated JSON formatiing improved.

--- os2borgerpc/chrome/chrome_add_remove_extension.sh
+++ os2borgerpc/chrome/chrome_add_remove_extension.sh
@@ -4,7 +4,7 @@
 
 # This script can:
 # 1. Create an ExtensionSettings policy if none exists.
-# 2. Adds/remove a list(1..*) of Chrome Extensions to/from the ExtensionSettings file.
+# 2. Add/remove a list(1..*) of Chrome Extensions to/from the ExtensionSettings file.
 # 3. Remove the ExtensionSettings policy.
 
 # Authors: Heini Leander Ovason
@@ -26,18 +26,24 @@ if [ "$ACTIVATE" = 'True' ]; then
   EXTENSIONS_DICT=""
   if [ -n "$EXTENSIONS_ARRAY" ]; then
     IFS=',' read -ra EXTENSIONS_ARRAY <<< "$EXTENSIONS_ARRAY"
+    ARR_LEN="${#EXTENSIONS_ARRAY[@]}"
 
+    C=0
     for EXTENSION in "${EXTENSIONS_ARRAY[@]}"
     do
       DICT_TEMPLATE="\"$EXTENSION\": {
       \"installation_mode\": \"force_installed\",
       \"toolbar_pin\": \"force_pinned\",
-      \"update_url\":
-      \"https://clients2.google.com/service/update2/crx\"
+      \"update_url\": \"https://clients2.google.com/service/update2/crx\"
     }"
-      EXTENSIONS_DICT+="$DICT_TEMPLATE,"
+      C=$((C+1))
+      if [ "$C" -eq "$ARR_LEN" ]; then
+        EXTENSIONS_DICT+="    $DICT_TEMPLATE"
+      else
+        EXTENSIONS_DICT+="$DICT_TEMPLATE,
+"
+      fi
     done
-    EXTENSIONS_DICT=${EXTENSIONS_DICT::-1} # remove comma at end
   fi
 
   cat << EOF > "$POLICIES_DIR/$POLICY_FILE"
