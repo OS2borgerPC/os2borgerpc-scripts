5fb0d3ed8196340ee4adde48a1bf75cff3f4e13e
Marcus Funch <mfm@magenta.dk>
2023-01-12 16:11:48 +0100

    Add the updated name of the gnome extension

--- os2borgerpc/custom/kobenhavn/cicero_install_pam/raw_scripts/cicero_install_pam_raw.sh
+++ os2borgerpc/custom/kobenhavn/cicero_install_pam/raw_scripts/cicero_install_pam_raw.sh
@@ -16,8 +16,9 @@ export DEBIAN_FRONTEND=noninteractive
 LIGHTDM_PAM=/etc/pam.d/lightdm
 # Put our module where PAM modules normally are
 PAM_PYTHON_MODULE=/usr/lib/x86_64-linux-gnu/security/os2borgerpc-cicero-pam-module.py
+# Keep this in sync with the extensions name as given in the os2borgerpc-gnome-extensions repo!
 # shellcheck disable=SC2034   # It exists in an included file
-EXTENSION_NAME='logout_timer@os2borgerpc.magenta.dk'
+EXTENSION_NAME='logout-timer@os2borgerpc.magenta.dk'
 # shellcheck disable=SC2034   # It exists in an included file
 LOGOUT_TIMER_CONF="/usr/share/gnome-shell/extensions/$EXTENSION_NAME/config.json"
 CICERO_INTERFACE_PYTHON3=/usr/share/os2borgerpc/bin/cicero_interface_python3.py
