3c01e91d7a8af41fbc3770ac13a6340e7a20a914
Andreas Poulsen <ap@magenta-aps.dk>
2023-01-31 11:21:43 +0100

    fix network manager script for ubuntu 22

--- os2borgerpc/os2borgerpc/network_manager_allow_user_changes.sh
+++ os2borgerpc/os2borgerpc/network_manager_allow_user_changes.sh
@@ -13,10 +13,15 @@ ACTIVATE="$1"
 # Note to future dev: Method attempted which proved unsuccessful:
 # 1. Add user to netdev, systemd-network or network groups
 FILE=/etc/NetworkManager/NetworkManager.conf
+FILE2=/var/lib/polkit-1/localauthority/50-local.d/networkmanager.pkla
 
 # Cleanup after previous runs of this script - or disable access if previously given (idempotency)
 sed --in-place '/auth-polkit=false/d' $FILE
+# Only make this replacement for user-related entries
+sed --in-place '/unix-group:user/{ n; n; n; n; s/ResultActive=yes/ResultActive=no/ }' $FILE2
 
 if [ "$ACTIVATE" = 'True' ]; then
   sed --in-place '/\[main\]/a\auth-polkit=false' $FILE
+  # Only make this replacement for user-related entries
+  sed --in-place '/unix-group:user/{ n; n; n; n; s/ResultActive=no/ResultActive=yes/ }' $FILE2
 fi
