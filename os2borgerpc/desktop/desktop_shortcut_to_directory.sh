efc866bb6632707526651b0bddf1c5f59cc5b1f7
Marcus Funch <mfm@magenta.dk>
2023-02-13 13:40:18 +0100

    Desktop shortcut to directory: Add support for removing shortcut

--- os2borgerpc/desktop/desktop_shortcut_to_directory.sh
+++ os2borgerpc/desktop/desktop_shortcut_to_directory.sh
@@ -3,17 +3,23 @@
 # Places a shortcut on the Desktop to any directory on the file system
 #
 # Parameters:
-#   1: The path to the directory you want a shortcut to
-#   2: The name of the shortcut on the Desktop
+#   1: Whether to add or remove the shortcut
+#   2: The path to the directory you want a shortcut to
+#   3: The name of the shortcut on the Desktop
 
 set -ex
 
-DIRECTORY="$1"
-SHORTCUT_NAME="$2"
+ADD="$1"
+DIRECTORY="$2"
+SHORTCUT_NAME="$3"
 
 SHADOW_DESKTOP="/home/.skjult/Skrivebord"
 
 mkdir --parents $SHADOW_DESKTOP
 
-# Note: "ln" doesn't care if the destination ($DIRECTORY) exists
-ln --symbolic --force "$DIRECTORY" "$SHADOW_DESKTOP/$SHORTCUT_NAME"
+if [ "$ADD" = "True" ]; then
+  # Note: "ln" doesn't care if the destination ($DIRECTORY) exists
+  ln --symbolic --force "$DIRECTORY" "$SHADOW_DESKTOP/$SHORTCUT_NAME"
+else
+  rm "$SHADOW_DESKTOP/$SHORTCUT_NAME"
+fi
