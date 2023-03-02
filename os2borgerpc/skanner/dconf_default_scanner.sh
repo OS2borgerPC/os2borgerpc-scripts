38742b40de15e4deed2049057e123bcd7b7a1077
Marcus Funch <mfm@magenta.dk>
2023-03-02 14:42:42 +0100

    Add quotes around value

--- os2borgerpc/skanner/dconf_default_scanner.sh
+++ os2borgerpc/skanner/dconf_default_scanner.sh
@@ -21,14 +21,14 @@ if [ "$ACTIVATE" = 'True' ]; then
 
 	# dconf does not, by default, require the use of a system database, so
 	# add one (called "os2borgerpc") to store our system-wide settings in
-	cat > "/etc/dconf/profile/user" <<-END
+	cat > "/etc/dconf/profile/user" <<- END
 		user-db:user
 		system-db:os2borgerpc
 	END
 
-	cat > "$POLICY_FILE" <<-END
+	cat > "$POLICY_FILE" <<- END
 		[$POLICY_PATH]
-		$POLICY=$POLICY_VALUE
+		$POLICY="$POLICY_VALUE"
 	END
 	# "dconf update" will only act if the content of the keyfile folder has
 	# changed: individual files changing are of no consequence. Force an update
@@ -37,7 +37,7 @@ if [ "$ACTIVATE" = 'True' ]; then
 
 	# Tell the system that the values of the dconf keys we've just set can no
 	# longer be overridden by the user
-	cat > "$POLICY_LOCK_FILE" <<-END
+	cat > "$POLICY_LOCK_FILE" <<- END
 		/$POLICY_PATH/$POLICY
 	END
 else
