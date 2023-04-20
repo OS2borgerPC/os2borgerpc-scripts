1abc7d63905f2b66729871d107492a160aec668f
Marcus Funch <mfm@magenta.dk>
2023-04-20 16:03:17 +0200

    Build in fix to prevent scanner popup in 22.04 into printer_toggle_network_discovery

--- os2borgerpc/printer/printer_toggle_network_discovery.sh
+++ os2borgerpc/printer/printer_toggle_network_discovery.sh
@@ -5,7 +5,7 @@ set -ex
 # Enable / Disable network printer discovery.
 # Use a boolean to enable or disable. A checked box will disable
 # network printer discovery and an unchecked one will enable it.
-# As a side effect all network printers previously found are removed 
+# As a side effect all network printers previously found are removed
 # and any you want, have to be added manually.
 # Log out or restart if changes don't take immediate effect.
 #
@@ -18,13 +18,26 @@ set -ex
 
 ACTIVATE=$1
 
+POLKIT_POLICY="/etc/polkit-1/localauthority/10-vendor.d/01-os2borgerpc-deny-user-managing-units.pkla"
+
 if [ "$ACTIVATE" = "True" ]; then
   # Disable network printer discovery
   systemctl mask avahi-daemon cups-browsed
   # Mask vs. disable: https://askubuntu.com/a/816378/284161
   systemctl stop avahi-daemon cups-browsed
 
+  cat <<- EOF > $POLKIT_POLICY
+		[User shan't manage units, to prevent simple-scan/saned from prompting for password trying to start avahi-daemon]
+		Identity=unix-user:user
+		Action=org.freedesktop.systemd1.manage-units
+		ResultAny=no
+		ResultInactive=no
+		ResultActive=no
+	EOF
+
 else # Enable network printer discovery
   systemctl unmask avahi-daemon cups-browsed
   systemctl start avahi-daemon cups-browsed
+
+  rm --force $POLKIT_POLICY
 fi
