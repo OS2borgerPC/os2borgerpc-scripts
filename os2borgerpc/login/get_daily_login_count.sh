7b26397751fb0d2748986d559f3c51a546c8e1bd
Andreas Poulsen <ap@magenta-aps.dk>
2023-07-04 14:37:30 +0200

    Expand daily login count script to append new counts to existing ones

--- os2borgerpc/login/get_daily_login_count.sh
+++ os2borgerpc/login/get_daily_login_count.sh
@@ -4,6 +4,7 @@ set -x
 
 LOGIN_COUNT_SCRIPT="/usr/local/lib/os2borgerpc/count_daily_logins.sh"
 LOGIN_COUNT_SERVICE="/etc/systemd/system/os2borgerpc-count_daily_logins.service"
+CONFIG_NAME="login_counts"
 ROOTCRON_TMP="/etc/rootcron"
 
 mkdir --parents "$(dirname $LOGIN_COUNT_SCRIPT)"
@@ -22,17 +23,27 @@ cat <<EOF > $LOGIN_COUNT_SCRIPT
 #!/usr/bin/env bash
 
 YESTERDAY_DATE=\$(LANG=en_US.UTF-8 date -d "yesterday" "+%b %_d")
+YESTERDAY_FULL_DATE=\$(date -d "yesterday" +%F)
 LOG_FILE="/var/log/auth.log"
 
-if ! grep --quiet "$YESTERDAY_DATE" \$LOG_FILE; then
+OLD_LOGIN_COUNTS=\$(get_os2borgerpc_config "$CONFIG_NAME")
+
+if ! grep --quiet "\$YESTERDAY_DATE" \$LOG_FILE; then
   LOG_FILE="var/log/auth.log.1"
 fi
 
 LOGIN_COUNT=\$(grep --text "\$YESTERDAY_DATE" "\$LOG_FILE" | grep -c "New session c[^ ]* of user user")
 
-set_os2borgerpc_config logins_yesterday \$LOGIN_COUNT
+if [ -z "\$OLD_LOGIN_COUNTS" ]; then
+  CONFIG_VALUE=\$(echo "\$YESTERDAY_FULL_DATE: \$LOGIN_COUNT")
+else
+  CONFIG_VALUE=\$(echo "\$OLD_LOGIN_COUNTS, \$YESTERDAY_FULL_DATE: \$LOGIN_COUNT")
+fi
 
-os2borgerpc_push_config_keys logins_yesterday
+if ! grep --quiet "\$YESTERDAY_FULL_DATE" <<< "\$OLD_LOGIN_COUNTS"; then
+  set_os2borgerpc_config "$CONFIG_NAME" "\$CONFIG_VALUE"
+  os2borgerpc_push_config_keys "$CONFIG_NAME"
+fi
 EOF
 
 chmod 700 $LOGIN_COUNT_SCRIPT
@@ -49,4 +60,4 @@ ExecStart=$LOGIN_COUNT_SCRIPT
 WantedBy=multi-user.target
 EOF
 
-systemctl enable "$(basename $LOGIN_COUNT_SERVICE)"
+systemctl enable --now "$(basename $LOGIN_COUNT_SERVICE)"
