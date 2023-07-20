349af5909b424ed4e3b29882ab0e15a10d278484
Andreas Poulsen <ap@magenta-aps.dk>
2023-07-20 09:57:05 +0200

    Rewrite touchscreen coordinates script to always use transformation matrices

--- os2borgerpc_kiosk/os2borgerpc_kiosk/touchscreen_fix_coordinates_issues.sh
+++ os2borgerpc_kiosk/os2borgerpc_kiosk/touchscreen_fix_coordinates_issues.sh
@@ -12,29 +12,27 @@ set -x
 
 ACTIVATE="$1"
 INVERT="$2"
-SWAP="$3"
+ORIENTATION="$3"
 
 # Ubuntus builtin Xorgs configs are in /usr/share/X11/xorg.conf.d - but user overrides usually go in /etc.
 # Both dirs are read by Xorg.
 OS2BPC_EVDEV_FILE=/etc/X11/xorg.conf.d/90-os2bpc-evdev.conf
 
-if [ "$INVERT" = "True" ]; then
-  # The indentation is kinda weird in this variable, but it makes it match up in the output file (8 spaces in front)
-  INVERT_TEXT='Option "InvertY" "true"
-        Option "InvertX" "true"'
+if [ -z "$ORIENTATION" ]; then
+  ORIENTATION="normal"
 fi
 
-if [ "$SWAP" = "True" ]; then
-  SWAP_TEXT='Option "TransformationMatrix" "0 1 0 -1 0 1 0 0 1"'
-fi
-
-if [ "$INVERT" = "True" ] && [ "$SWAP" = "True" ]; then
-  TEXT="$INVERT_TEXT
-        $SWAP_TEXT"
-elif [ "$INVERT" = "True" ] && [ "$SWAP" = "False" ]; then
-  TEXT="$INVERT_TEXT"
-elif [ "$INVERT" = "False" ] && [ "$SWAP" = "True" ]; then
-  TEXT="$SWAP_TEXT"
+# To understand the necessary transformation matrix, perform the following thought experiment:
+# Imagine the coordinate system that represents the intended operation of the screen,
+# then determine how this must be rotated to match the coordinate system that would represent
+# the operation of the screen if its orientation was normal. If the axes also need to be inverted,
+# rotate by an extra 180 degrees. The necessary transformation matrix represents the total rotation
+if [ "$INVERT" = "True" ] && [ "$ORIENTATION" = "normal" ]; then
+  TEXT='Option "TransformationMatrix" "-1 0 1 0 -1 1 0 0 1"'
+elif { [ "$INVERT" = "True" ] && [ "$ORIENTATION" = "left" ]; } || { [ "$INVERT" = "False" ] && [ "$ORIENTATION" = "right" ]; }; then
+  TEXT='Option "TransformationMatrix" "0 1 0 -1 0 1 0 0 1"'
+elif { [ "$INVERT" = "True" ] && [ "$ORIENTATION" = "right" ]; } || { [ "$INVERT" = "False" ] && [ "$ORIENTATION" = "left" ]; }; then
+  TEXT='Option "TransformationMatrix" "0 -1 1 1 0 0 0 0 1"'
 else
   TEXT=""
 fi
