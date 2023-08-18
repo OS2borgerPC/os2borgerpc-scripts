3cb2ded638e3c2cbaef54140f2a9634a5ddf0c13
Marcus Funch <mfm@magenta.dk>
2023-08-18 15:00:35 +0200

    HP USB Scanner setup script: Simplify away from regexes

--- os2borgerpc/skanner/hp-usb-scanner-setup.sh
+++ os2borgerpc/skanner/hp-usb-scanner-setup.sh
@@ -3,26 +3,26 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # Install the dependencies to run the interactive script noninteractively
-apt-get update -y
-apt-get install -yf expect
+apt-get update --assume-yes
+apt-get install --assume-yes --fix-broken expect
 
 SCRIPT_PATH=/tmp/hp-plugin-setup.sh
 
 cat << EOF > $SCRIPT_PATH
 #!/usr/bin/expect -f
- 
+
 set timeout -1
- 
+
 spawn hp-plugin -i
- 
-expect -re "Enter option \\\\(d\\\\=download\\\\*, p\\\\=specify path, q\\\\=quit\\\\) \\\\? "
- 
+
+expect "Enter option (d=download*, p=specify path, q=quit) ? "
+
 send -- "\r"
- 
-expect -re "Do you accept the license terms for the plug-in \\\\(y\\\\=yes\\\\*, n\\\\=no, q\\\\=quit\\\\) \\\\? "
- 
+
+expect "Do you accept the license terms for the plug-in (y=yes*, n=no, q=quit) ? "
+
 send -- "\r"
- 
+
 expect eof
 EOF
 
@@ -30,4 +30,4 @@ EOF
 chmod +x $SCRIPT_PATH
 
 # Run it
-/tmp/hp-plugin-setup.sh
+$SCRIPT_PATH
