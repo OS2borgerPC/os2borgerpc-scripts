02530b5d5950c7994336f9b71e96b0072b964be2
Andreas Poulsen <ap@magenta-aps.dk>
2023-09-05 09:00:05 +0200

    Add script to install a chosen app

---
+++ common/system/install_app.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -x
+
+INSTALL=$1
+APPNAME=$2
+
+# Resync the local package index from its remote counterpart
+apt-get --assume-yes update
+# Attempt to fix broken or interrupted installations
+apt-get --assume-yes --fix-broken install
+
+# Install or remove the chosen package
+if [ "$INSTALL" = "True" ]; then
+  apt-get --assume-yes install "$APPNAME"
+else
+  apt-get --assume-yes remove "$APPNAME"
+fi
+
+# Remove packages only installed as dependencies, which are no longer dependencies
+apt-get --assume-yes autoremove
+# Remove local repository of retrieved package files
+apt-get --assume-yes clean
