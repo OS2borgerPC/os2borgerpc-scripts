87199011b325f7fd9f2c9d8e6e047df8f2b38bb0
Marcus Funch <mfm@magenta.dk>
2023-10-03 16:04:33 +0200

    LightDM fix boot error script rewritten to use a drop-in file

--- os2borgerpc/os2borgerpc/lightdm_fix_boot_error.sh
+++ os2borgerpc/os2borgerpc/lightdm_fix_boot_error.sh
@@ -2,24 +2,22 @@
 
 set -x
 
+if get_os2borgerpc_config os2_product | grep --quiet kiosk; then
+  echo "Dette script er ikke designet til at blive anvendt på en kiosk-maskine."
+  exit 1
+fi
+
 ACTIVATE="$1"
 
-LIGHTDM_CONF="/etc/lightdm/lightdm.conf"
+CONF="/etc/lightdm/lightdm.conf.d/login-check-graphical.conf"
 
 if [ "$ACTIVATE" = "True" ]; then
 
-  if ! grep -q -- "\[LightDM\]" "$LIGHTDM_CONF"; then
-cat << EOF >> $LIGHTDM_CONF
-
+cat << EOF > $CONF
 [LightDM]
 logind-check-graphical=true
 EOF
-  else
-    echo "It appears as if the config file already contains the change?"
-    exit 1
-  fi
 
 else
-  # Delete the above lines (currently except the empty line)
-  sed --in-place --expression '/\[LightDM\]/d' --expression '/logind-check-graphical/d' $LIGHTDM_CONF
+  rm --force $CONF
 fi
