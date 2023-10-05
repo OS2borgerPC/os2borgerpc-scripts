53b897a1252123342a00490984f1d9a8cb7c0c87
Andreas Poulsen <ap@magenta-aps.dk>
2023-10-05 11:58:41 +0200

    Ensure that login_counts for a given date is not reported until a later date

--- os2borgerpc/login/get_daily_login_count.sh
+++ os2borgerpc/login/get_daily_login_count.sh
@@ -7,14 +7,23 @@ LOGIN_COUNT_SERVICE="/etc/systemd/system/os2borgerpc-count_daily_logins.service"
 DATE_FILE="/etc/os2borgerpc/last_on_date.txt"
 CONFIG_NAME="login_counts"
 DATA_LIMIT=89 # This is one less than the number of days that are stored
-ROOTCRON_TMP="/etc/rootcron"
+ROOTCRON_TMP="/tmp/rootcron"
 
-mkdir --parents "$(dirname $LOGIN_COUNT_SCRIPT)"
-mkdir --aprents "$(dirname $DATE_FILE)"
+ACTIVATE=$1
+
+mkdir --parents "$(dirname $LOGIN_COUNT_SCRIPT)" "$(dirname $DATE_FILE)"
 
 crontab -l > $ROOTCRON_TMP
 
 sed -i "/count_daily_logins/d" $ROOTCRON_TMP
+
+if [ "$ACTIVATE" = "False" ]; then
+  systemctl disable "$(basename $LOGIN_COUNT_SERVICE)"
+  crontab $ROOTCRON_TMP
+  rm --force $LOGIN_COUNT_SCRIPT $LOGIN_COUNT_SERVICE \
+              $DATE_FILE $ROOTCRON_TMP
+  exit 0
+fi
 
 echo "* 4 * * * $LOGIN_COUNT_SCRIPT" >> $ROOTCRON_TMP
 
@@ -22,24 +31,30 @@ crontab $ROOTCRON_TMP
 
 rm --force $ROOTCRON_TMP
 
+# When the script is run, get value for the day before
+# This might not be necessary, but it's convenient for testing
 date -d "yesterday" +%F > $DATE_FILE
 
 cat <<EOF > $LOGIN_COUNT_SCRIPT
 #!/usr/bin/env bash
 
 LAST_ON_DATE_FULL=\$(cat $DATE_FILE)
+# Convert to the date format used in auth.log
 LAST_ON_DATE=\$(LANG=en_US.UTF-8 date -d "\$LAST_ON_DATE_FULL" "+%b %_d")
 TODAY_DATE_FULL=\$(date -d "today" +%F)
 echo \$TODAY_DATE_FULL > $DATE_FILE
 
-YESTERDAY_DATE=\$(LANG=en_US.UTF-8 date -d "yesterday" "+%b %_d")
-YESTERDAY_FULL_DATE=\$(date -d "yesterday" +%F)
+# Stop if the date to be checked is today
+if [ "\$LAST_ON_DATE_FULL" = "\$TODAY_DATE_FULL" ]; then
+  exit 0
+fi
+
 LOG_FILE="/var/log/auth.log"
 
 OLD_LOGIN_COUNTS=\$(get_os2borgerpc_config "$CONFIG_NAME")
 
 if ! grep --quiet "\$LAST_ON_DATE" \$LOG_FILE; then
-  LOG_FILE="var/log/auth.log.1"
+  LOG_FILE="/var/log/auth.log.1"
 fi
 
 LOGIN_COUNT=\$(grep --text "\$LAST_ON_DATE" "\$LOG_FILE" | grep -c "New session c[^ ]* of user user")
@@ -47,17 +62,11 @@ LOGIN_COUNT=\$(grep --text "\$LAST_ON_DATE" "\$LOG_FILE" | grep -c "New session 
 if [ -z "\$OLD_LOGIN_COUNTS" ]; then
   CONFIG_VALUE=\$(echo "\$LAST_ON_DATE_FULL: \$LOGIN_COUNT")
 else
+  # Remove old values to ensure that we never save more than DATA_LIMIT+1 days
   IFS="," read -ra OLD_COUNTS_ARRAY <<< "\$(echo "\$OLD_LOGIN_COUNTS" | sed "s/, /,/g")"
-  IFS=":" read -ra NEWEST_OLD_COUNT <<< \${OLD_COUNTS_ARRAY[-1]}
-  if [ \${NEWEST_OLD_COUNT[0]} = \$LAST_ON_DATE_FULL ] && [ \${NEWEST_OLD_COUNT[1]} -lt \$LOGIN_COUNT ]; then
-    TEMP=\$(IFS="," ; echo "\${OLD_COUNTS_ARRAY[*]::\${#OLD_COUNTS_ARRAY[@]}-1}")
-    IFS="," read -ra OLD_COUNTS_ARRAY <<< \$TEMP
-  fi
   if [ \${#OLD_COUNTS_ARRAY[@]} -gt $DATA_LIMIT ]; then
-    TEMP=\$(IFS="," ; echo "\${OLD_COUNTS_ARRAY[*]: -$DATA_LIMIT}")
-    IFS="," read -ra OLD_COUNTS_ARRAY <<< \$TEMP
+    OLD_LOGIN_COUNTS=\$(IFS="," ; echo "\${OLD_COUNTS_ARRAY[*]: -$DATA_LIMIT}" | sed "s/,/, /g")
   fi
-  OLD_LOGIN_COUNTS=\$(IFS="," ; echo "\${OLD_COUNTS_ARRAY[*]}" | sed "s/,/, /g")
   CONFIG_VALUE=\$(echo "\$OLD_LOGIN_COUNTS, \$LAST_ON_DATE_FULL: \$LOGIN_COUNT")
 fi
 
