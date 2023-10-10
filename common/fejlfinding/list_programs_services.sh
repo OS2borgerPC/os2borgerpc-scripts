c3ab6eca0d9c12841d5125a6e5450b631543ffbb
Marcus Funch <mfm@magenta.dk>
2023-10-10 16:41:36 +0200

    Don't show uninstalled packages in the installed packages script

--- common/fejlfinding/list_programs_services.sh
+++ common/fejlfinding/list_programs_services.sh
@@ -2,7 +2,7 @@
 
 printf "\n\n%s\n\n" "===== LIST INSTALLED PROGRAMS: $0 ====="
 #apt list --installed
-dpkg -l
+dpkg -l | grep --invert-match "^rc"  # Don't show packages that WERE installed
 
 printf "\n\n%s\n\n" "===== LIST ALL SERVICES: $0 ====="
 systemctl list-units
