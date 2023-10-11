80104d3e4c1439b22d385df43eec4fc24188fbd3
Marcus Funch <mfm@magenta.dk>
2023-10-11 13:46:48 +0200

    GRUB menu script: Make the time to wait configurable

--- common/system/grub_menu_make_accessible.sh
+++ common/system/grub_menu_make_accessible.sh
@@ -1,25 +1,26 @@
 #! /usr/bin/env sh
 
 GRUB_MENU_ACCESSIBLE="$1"
-
-set -x
+WAIT_HOW_LONG="$2"  # set to -1 to wait forever
 
 GRUB_CONFIG="/etc/default/grub"
 
-echo "Show full GRUB config before"
-cat $GRUB_CONFIG
+set -x
 
-if [ "$GRUB_MENU_ACCESSIBLE" = "True" ]; then
-    sed --in-place "s/GRUB_TIMEOUT_STYLE=hidden/GRUB_TIMEOUT_STYLE=menu/" $GRUB_CONFIG
-    sed --in-place "s/GRUB_TIMEOUT=0/GRUB_TIMEOUT=40/" $GRUB_CONFIG
-else
-    sed --in-place "s/GRUB_TIMEOUT_STYLE=menu/GRUB_TIMEOUT_STYLE=hidden/" $GRUB_CONFIG
-    sed --in-place "s/GRUB_TIMEOUT=30/GRUB_TIMEOUT=0/" $GRUB_CONFIG
-fi
-
-echo "Show relevant settings after:"
+echo "Show relevant settings before:"
 grep "TIMEOUT_STYLE" $GRUB_CONFIG
 grep "GRUB_TIMEOUT=" $GRUB_CONFIG
 
-# Now update GRUB with the new settings
+if [ "$GRUB_MENU_ACCESSIBLE" = "True" ]; then
+  sed --in-place "s/GRUB_TIMEOUT_STYLE=hidden/GRUB_TIMEOUT_STYLE=menu/" $GRUB_CONFIG
+  sed --in-place "s/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=$WAIT_HOW_LONG/" $GRUB_CONFIG
+else
+  sed --in-place "s/GRUB_TIMEOUT_STYLE=menu/GRUB_TIMEOUT_STYLE=hidden/" $GRUB_CONFIG
+  sed --in-place "s/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/" $GRUB_CONFIG
+fi
+
+echo "Show full GRUB config after:"
+cat $GRUB_CONFIG
+
+# Now update GRUB's actual configuration with the new settings
 update-grub
