8f4d4f5b559c4fb7f9ba57116d078366afa8bd04
Marcus Funch <mfm@magenta.dk>
2023-10-16 11:55:44 +0200

    fixup! Make script to add shortcut to autostart global

--- os2borgerpc/os2borgerpc/copy_shortcut_to_autostart.sh
+++ os2borgerpc/os2borgerpc/copy_shortcut_to_autostart.sh
@@ -4,8 +4,17 @@ set -ex
 
 SHORTCUT_NAME="$1"
 
+# Determine the name of the user desktop directory. This is done via xdg-user-dir,
+# which checks the /home/user/.config/user-dirs.dirs file. To ensure this file exists,
+# we run xdg-user-dirs-update, which generates it based on the environment variable
+# LANG. This variable is empty in lightdm so we first export it
+# based on the value stored in /etc/default/locale
+export "$(grep LANG= /etc/default/locale | tr -d '"')"
+runuser -u user xdg-user-dirs-update
+DESKTOP=$(basename "$(runuser -u user xdg-user-dir DESKTOP)")
+
 SHADOW_USER=".skjult"
-SHADOW_DESKTOP="/home/$SHADOW_USER/Skrivebord"
+SHADOW_DESKTOP="/home/$SHADOW_USER/$DESKTOP"
 AUTOSTART_DIR="/home/$SHADOW_USER/.config/autostart"
 
 mkdir --parents $AUTOSTART_DIR
