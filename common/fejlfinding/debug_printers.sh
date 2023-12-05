83d34569454ac65a1654c9d2f879e3434e8f460b
Marcus Funch <mfm@magenta.dk>
2023-12-05 15:05:32 +0100

    Printers: Set page size in a different way, gather more info in infoscript

---
+++ common/fejlfinding/debug_printers.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env sh
+
+header() {
+  MSG=$1
+  printf "\n\n\n%s\n\n\n" "### $MSG: ###"
+}
+
+text() {
+  MSG=$1
+  printf "\n%s\n" "### $MSG: ###"
+}
+
+text "Check the version of hplip"
+dpkg -l hplip | cat  # Piping to cat because otherwise it seems to open "less"
+
+text "Info about currently added printers"
+lpstat -v
+
+text "Global standard paper size is set to"
+# "Paperconf prints  the  name  of the
+# the  system-  or  user-specified paper, obtained by looking in order at
+# the PAPERSIZE environment variable, at the contents of the file  speci-
+# fied by the PAPERCONF environment variable, at the contents of /etc/pa-
+# persize or by using letter as a fall-back value if none  of  the  other
+# alternatives are successful"
+paperconf
+# The related command "paperconfig" can set the default paper size.
+
+header "Current printer settings for all added printers"
+
+for printer in $(lpstat -a | cut  --delimiter ' ' --fields 1); do
+  text "Printer name: \"$printer\" has these options set:"
+  lpoptions -l -p "$printer"
+  echo ""
+done
