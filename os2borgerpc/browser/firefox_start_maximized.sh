45b812a448ec5bee6efbf682850d3c5b6c9a06c8
Marcus Funch <mfm@magenta.dk>
2024-01-03 11:23:55 +0100

    Add script to start Firefox maximized

---
+++ os2borgerpc/browser/firefox_start_maximized.sh
@@ -0,0 +1,59 @@
+#! /usr/bin/env sh
+#
+ACTIVATE="$1"
+
+FIREFOX_DESKTOP_SNAP="/var/lib/snapd/desktop/applications/firefox_firefox.desktop"
+FIREFOX_DESKTOP_APT="/usr/share/applications/firefox.desktop"
+# Set to be a 16:9 resolution that's hopefully at or above the monitor resolution. GNOME should automatically resize it to fit.
+LAUNCH_ARGS="-width 7680 -height 4320"
+
+# Takes a parameter to add to the Exec lines of the desktop files passed as the subsequent arguments
+add_to_desktop_files() {
+  PARAMETER="$1"
+  shift # Now remove the parameter so we can loop over what remains: The files
+  for FILE in "$@"; do
+    # Only continue if the particular file exists
+    if [ -f "$FILE" ]; then
+      # Don't add the parameter multiple times
+      if ! grep --quiet -- "$PARAMETER" "$FILE"; then
+        sed --in-place "s,\(Exec=\S*\)\(.*\),\1 $PARAMETER\2," "$FILE"
+      fi
+    fi
+  done
+}
+
+# The Firefox Snap desktop file is quite differently formatted compared to a its non-Snap Exec line. Example:
+# Exec=env BAMF_DESKTOP_FILE_HINT=/var/lib/snapd/desktop/applications/firefox_firefox.desktop /snap/bin/firefox %u
+# Considering whether it would be better to copy the desktop file to ~/.local/share/applications/ and modify it there.
+add_to_desktop_files_ff_snap() {
+  PARAMETER="$1"
+  shift # Now remove the parameter so we can loop over what remains: The files
+  for FILE in "$@"; do
+    # Only continue if the particular file exists
+    if [ -f "$FILE" ]; then
+      # Don't add the parameter multiple times
+      if ! grep --quiet -- "$PARAMETER" "$FILE"; then
+        sed --in-place "s,\(.*/snap/bin/firefox\)\(.*\),\1 $PARAMETER\2," "$FILE"
+      fi
+    fi
+  done
+}
+
+if [ -d "/snap/firefox" ]; then
+  FIREFOX_DESKTOP=$FIREFOX_DESKTOP_SNAP
+else
+  FIREFOX_DESKTOP=$FIREFOX_DESKTOP_APT
+fi
+
+if [ "$ACTIVATE" = "True" ]; then
+  if [ -d "/snap/firefox" ]; then
+    add_to_desktop_files_ff_snap "$LAUNCH_ARGS" $FIREFOX_DESKTOP
+  else
+    add_to_desktop_files "$LAUNCH_ARGS" $FIREFOX_DESKTOP
+  fi
+else
+  sed --in-place "s/$LAUNCH_ARGS //" $FIREFOX_DESKTOP
+fi
+
+# This step doesn't seem necessary on 22.04, but it is on 20.04
+dconf update
