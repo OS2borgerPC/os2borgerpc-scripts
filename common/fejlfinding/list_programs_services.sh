800f13525614bb469f271cc2e0c2ec980a0eca10
Marcus Funch <mfm@magenta.dk>
2024-02-01 13:32:27 +0100

    Make the debugging script that lists programs list contents in /usr/share/applications as well

--- common/fejlfinding/list_programs_services.sh
+++ common/fejlfinding/list_programs_services.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env sh
 
-printf "\n\n%s\n\n" "===== LIST INSTALLED PROGRAMS: $0 ====="
+printf "\n\n%s\n\n" "===== LIST INSTALLED PACKAGES (PROGRAMS): $0 ====="
 #apt list --installed
 dpkg -l | grep --invert-match "^rc"  # Don't show packages that WERE installed
 
 printf "\n\n%s\n\n" "===== LIST ALL SERVICES: $0 ====="
 systemctl list-units
+
+printf "\n\n%s\n\n" "===== LIST PROGRAMS IN /usr/share/applications: $0 ====="
+ls -l /usr/share/applications
