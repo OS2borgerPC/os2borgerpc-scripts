e59ea4f2a6144316dac7a01be8e706eb7ec30f98
Andreas Poulsen <ap@magenta-aps.dk>
2024-02-13 14:31:18 +0100

    Update browser_set_default to use /etc/xdg/mimeapps.list

--- os2borgerpc/browser/browser_set_default.sh
+++ os2borgerpc/browser/browser_set_default.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 # Sets the default browser on a OS2borgerPC for the regular user
 #
@@ -18,29 +18,35 @@ lower() {
 
 BROWSER="$(lower "$1")"
 
-FILE="/usr/share/applications/defaults.list"
+FILE="/etc/xdg/mimeapps.list"
 
 # They can type in "chrome" but the desktop file is called google-chrome
+# They can type in "edge" but the desktop file is called microsoft-edge
 if [ "$BROWSER" = "chrome" ]; then
   BROWSER=google-chrome
+elif [ "$BROWSER" = "edge" ]; then
+  BROWSER=microsoft-edge
 fi
 
 # Handle snaps, which have names like firefox_firefox.desktop
 if [ -d "/snap/$BROWSER" ]; then
-	DESKTOP_FILE=${BROWSER}_$BROWSER.desktop
+  DESKTOP_FILE=${BROWSER}_$BROWSER.desktop
 else
-	DESKTOP_FILE=${BROWSER}.desktop
+  DESKTOP_FILE=${BROWSER}.desktop
 fi
 
-# We cleanup the defaults.list as sometimes it seems to be populated
-# with multiple lines for the same MIME type, which messes things up!
-sed -i "\@text/html\|application/xhtml+xml\|application/xml\|x-scheme-handler/http\|x-scheme-handler/https@d" $FILE
-
+# Make sure the file exists and has the correct header
+if [ ! -f "$FILE" ]; then
+  cat << EOF > $FILE
+[Default Applications]
+EOF
+fi
+# Cleanup the file to prevent duplicate lines
+sed -i "\@text/html\|application/xhtml+xml\|x-scheme-handler/http\|x-scheme-handler/https@d" $FILE
 # Now set the new default:
-cat <<- EOF >> $FILE
-	application/xhtml+xml=$DESKTOP_FILE
-	text/html=$DESKTOP_FILE
-	application/xml=$DESKTOP_FILE
-	x-scheme-handler/http=$DESKTOP_FILE
-	x-scheme-handler/https=$DESKTOP_FILE
+cat << EOF >> $FILE
+application/xhtml+xml=$DESKTOP_FILE
+text/html=$DESKTOP_FILE
+x-scheme-handler/http=$DESKTOP_FILE
+x-scheme-handler/https=$DESKTOP_FILE
 EOF
