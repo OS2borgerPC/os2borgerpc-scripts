1b492f214c87b2ae8143c7a19fe767127209e22d
Andreas Poulsen <ap@magenta-aps.dk>
2024-03-06 10:46:54 +0100

    Remove filtering of old security events

--- common/security/ping_event.py
+++ common/security/ping_event.py
@@ -9,18 +9,15 @@ from datetime import datetime
 def csv_writer(security_events):
     """Write security events to security events file."""
     with open("/etc/os2borgerpc/security/securityevent.csv", "at") as csvfile:
-        for timestamp, security_problem_uid, log_event, complete_log in security_events:
+        for timestamp, security_problem_uid, log_event in security_events:
             event_line = log_event.replace("\n", " ").replace("\r", "").replace(",", "")
-            csvfile.write(
-                f"{timestamp},{security_problem_uid},{event_line},{complete_log}\n"
-            )
+            csvfile.write(f"{timestamp},{security_problem_uid},{event_line}\n")
 
 
 now = datetime.now()
 timestamp = datetime.strftime(now, "%Y%m%d%H%M%S")
 security_problem_uid_template_var = "%SECURITY_PROBLEM_UID%"
 log_event = "Ping warning."
-complete_log = "Nothing to worry about. Just a ping warning."
 
 
-csv_writer([(timestamp, security_problem_uid_template_var, log_event, complete_log)])
+csv_writer([(timestamp, security_problem_uid_template_var, log_event)])
