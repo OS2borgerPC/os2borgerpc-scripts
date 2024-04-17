a634f67d144c8589f4cd40a95b7a1bf45437e592
Andreas Poulsen <ap@magenta-aps.dk>
2024-04-17 14:43:12 +0200

    Add option to printer adding scripts to set added printer as standard

--- os2borgerpc/printer/network_printer_add_ppd.sh
+++ os2borgerpc/printer/network_printer_add_ppd.sh
@@ -8,5 +8,11 @@ HOST="$2"
 DESCRIPTION="$3"
 DRIVER="$4"
 PROTOCOL="${5:-socket}"
+SET_STANDARD="$6"
 
 lpadmin -p "$NAME" -v "$PROTOCOL://$HOST" -D "$DESCRIPTION" -E -P "$DRIVER" -L "$DESCRIPTION"
+
+if [ "$SET_STANDARD" = "True" ]; then
+  # Set the printer as standard printer
+  lpadmin -d "$NAME" && lpstat -d
+fi
