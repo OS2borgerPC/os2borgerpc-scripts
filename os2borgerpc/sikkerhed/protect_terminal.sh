5c795305fc658b4150440aa3cf1e61efc0073999
Marcus Funch <mfm@magenta.dk>
2024-05-16 19:10:33 +0200

    Rewrite protect_terminal to only use statoverride and hide the shortcut

--- os2borgerpc/sikkerhed/protect_terminal.sh
+++ os2borgerpc/sikkerhed/protect_terminal.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -ex
 
@@ -8,59 +8,52 @@ if get_os2borgerpc_config os2_product | grep --quiet kiosk; then
 fi
 
 ACTIVATE=$1
-PROGRAM_OLD_PATH="/usr/bin/gnome-terminal"
-PROGRAM_NEW_PATH="$PROGRAM_OLD_PATH.real"
+PROGRAM_PATH="/usr/bin/gnome-terminal"
 
-# Also remove the gnome extension that can start gnome terminal, suppress errors
+SKEL=".skjult"
+SHORTCUT_NAME="org.gnome.Terminal.desktop"
+SHORTCUT_GLOBAL_PATH="/usr/share/applications/$SHORTCUT_NAME"
+SHORTCUT_LOCAL_PATH="/home/$SKEL/.local/share/applications/$SHORTCUT_NAME"
+
+# Also remove the gnome extension that can start gnome terminal, don't stop execution if it fails
 apt-get remove --assume-yes nautilus-extension-gnome-terminal || true
 
-# Restore access
-if [ "$ACTIVATE" = 'True' ]; then
-  # Making sure we're not removing the actual
-  # gnome-terminal if run with the wrong argument or multiple times
-  if grep --quiet 'zenity' "$PROGRAM_OLD_PATH"; then
-    # Remove the permissions override and manually reset permissions to defaults
-     # Suppress error to prevent set -e exiting in case the override no longer exists
-    dpkg-statoverride --remove "$PROGRAM_NEW_PATH" || true
-    chown root:root "$PROGRAM_OLD_PATH"
-    chmod 755 "$PROGRAM_NEW_PATH"
-    # Remove the shell script that prints the error message
-    rm "$PROGRAM_OLD_PATH"
-    # Remove location override and restore gnome-terminal.real back to gnome-terminal
-    dpkg-divert --remove --no-rename "$PROGRAM_OLD_PATH"
-    # dpkg-divert can --rename it itself, but the problem with doing that is that in some images
-    # dpkg-divert is not used, it was simply moved/copied, so that won't restore it, leaving you
-    # with no gnome-control-center
-    mv "$PROGRAM_NEW_PATH" "$PROGRAM_OLD_PATH"
-  fi
+# Backwards compatibility - undo the effects of the previous script versions:
+# Making sure we're not removing the actual gnome-terminal
+if grep --quiet 'zenity' "$PROGRAM_PATH"; then
+  PROGRAM_HISTORICAL_PATH="$PROGRAM_PATH.real"
+
+  dpkg-statoverride --remove "$PROGRAM_PATH" || true
+  # Remove the shell script that prints the error message
+  rm "$PROGRAM_PATH"
+  # Remove location override and restore gnome-terminal.real back to gnome-terminal
+  dpkg-divert --remove --no-rename "$PROGRAM_PATH"
+  # dpkg-divert can --rename it itself, but the problem with doing that is that in some images
+  # dpkg-divert is not used, it was simply moved/copied, so that won't restore it, leaving you
+  # with no gnome-control-center
+  mv "$PROGRAM_HISTORICAL_PATH" "$PROGRAM_PATH"
+fi
+
+
+if [ "$ACTIVATE" = "True" ]; then # Restore access
+  # Remove the permissions override and manually reset permissions to defaults
+  # Suppress error to prevent set -e exiting in case the override no longer exists
+  dpkg-statoverride --remove "$PROGRAM_PATH" || true
+  # statoverride remove can't change permissions and ownership back by itself currently, unfortunately
+  chown root:root "$PROGRAM_PATH"
+  chmod 755 "$PROGRAM_PATH"
+
+  rm --force $SHORTCUT_LOCAL_PATH
 else # Deny access
-
-  if [ ! -f "$PROGRAM_NEW_PATH" ] # Don't divert and statoverride if they've already been done (idempotency)
-  then
-      dpkg-divert --rename --divert  "$PROGRAM_NEW_PATH" --add "$PROGRAM_OLD_PATH"
-      dpkg-statoverride --update --add superuser root 770 "$PROGRAM_OLD_PATH"
+  if ! dpkg-statoverride --list | grep --quiet "$PROGRAM_PATH"; then # Don't statoverride if it's already been done (idempotency)
+      dpkg-statoverride --update --add superuser root 770 "$PROGRAM_PATH"
   fi
-
-	cat <<- EOF > "$PROGRAM_OLD_PATH"
-#!/bin/bash
-
-USER=\$(id -un)
-
-# Set the info text based on the chosen language
-if echo \$LANG | grep sv; then
-  INFO="Terminalen är inte tillgänglig för allmänheten."
-elif echo \$LANG | grep en; then
-  INFO="The terminal is not accessible to the public."
-else
-  INFO="Terminalen er ikke tilgængelig for publikum."
+  # Additionally remove the terminal from Borgers program list for UX/cosmetic reasons (rather than security)
+  mkdir --parents "$(dirname $SHORTCUT_LOCAL_PATH)"
+  cp $SHORTCUT_GLOBAL_PATH $SHORTCUT_LOCAL_PATH
+  chmod o-r $SHORTCUT_LOCAL_PATH
 fi
 
-if [ \$USER == "user" ]; then
-  zenity --info --text="\$INFO"
-else
-  $PROGRAM_NEW_PATH "\$@"
-fi
-EOF
-
-  chmod +x "$PROGRAM_OLD_PATH"
-fi
+# For manual verification that there are no terminal diversions, but possibly a statoverride:
+dpkg-divert --list | grep terminal || true
+dpkg-statoverride --list | grep terminal || true
