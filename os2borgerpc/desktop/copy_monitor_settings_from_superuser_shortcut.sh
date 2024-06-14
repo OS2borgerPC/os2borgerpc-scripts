ec28745357225364166346b79d93f634551de883
Marcus Funch <mfm@magenta.dk>
2024-06-14 17:53:27 +0200

    Rewrite script to copy monitor settings to use zenity

--- os2borgerpc/desktop/copy_monitor_settings_from_superuser_shortcut.sh
+++ os2borgerpc/desktop/copy_monitor_settings_from_superuser_shortcut.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env sh
 
+# Author: Marcus Funch (mfm@magenta.dk)
+# License: GPL
+
 ACTIVATE="$1"
 
 SKELETON_USER=".skjult"
@@ -9,13 +12,6 @@ MONITOR_SETTINGS_FILE_SKELETON="/home/$SKELETON_USER/.config/monitors.xml"
 MONITOR_SCRIPT="/usr/share/os2borgerpc/bin/monitor-settings-superuser-copy.sh"
 MONITOR_SUDOERS_SCRIPT="/etc/sudoers.d/monitor-script-nopasswd"
 
-# Determine the name of the user desktop directory. This is done via xdg-user-dir,
-# which checks the /home/user/.config/user-dirs.dirs file. To ensure this file exists,
-# we run xdg-user-dirs-update, which generates it based on the environment variable
-# LANG. This variable is empty in lightdm so we first export it
-# based on the value stored in /etc/default/locale
-export "$(grep LANG= /etc/default/locale | tr -d '"')"
-runuser -u $SUPERUSER xdg-user-dirs-update
 DESKTOP=$(basename "$(runuser -u $SUPERUSER xdg-user-dir DESKTOP)")
 
 MONITOR_SCRIPT_DESKTOP_FILE="/home/superuser/$DESKTOP/os2borgerpc-monitor-settings-superuser-copy.desktop"
@@ -29,12 +25,12 @@ cat << EOF > $MONITOR_SCRIPT
 #! /usr/bin/env sh
 
 # Support three different languages
-MSG_SUCCESS_EN="Monitor settings updated to reflect superuser's. Logout and in again, and the changes should take effect. Press Enter."
-MSG_SUCCESS_DA="Skærmindstillingerne er opdateret til at matche superusers. Logud og ind igen, og derefter burde ændringerne tage effekt. Tryk Enter."
-MSG_SUCCESS_SV="Skärminställningarna har uppdaterats för att matcha superuser. Logga ut och in igen, och sedan bör ändringarna träda i kraft. Tryck Enter."
-MSG_ERROR_EN="Couldn't find the monitor settings file for superuser. You must first make changes to the monitor settings for superuser before running this script. Press Enter."
-MSG_ERROR_DA="Kunne ikke finde skærmindstillingsfilen for superuser. Inden kørsel af dette script skal du først lave ændringer i skærmindstillingerne for superuser. Tryk Enter."
-MSG_ERROR_SV="Kunde inte hitta skärminställningsfil för superuser. Innan du kör det här skriptet måste du först göra ändringar i skärminställningarna för superuser. Tryck Enter."
+MSG_SUCCESS_EN="Monitor settings updated to reflect superuser's. Logout and in again, and the changes should take effect."
+MSG_SUCCESS_DA="Skærmindstillingerne er opdateret til at matche superusers. Logud og ind igen, og derefter burde ændringerne tage effekt."
+MSG_SUCCESS_SV="Skärminställningarna har uppdaterats för att matcha superuser. Logga ut och in igen, och sedan bör ändringarna träda i kraft."
+MSG_ERROR_EN="Couldn't find the monitor settings file for superuser. You must first make changes to the monitor settings for superuser before running this script."
+MSG_ERROR_DA="Kunne ikke finde skærmindstillingsfilen for superuser. Inden kørsel af dette script skal du først lave ændringer i skærmindstillingerne for superuser."
+MSG_ERROR_SV="Kunde inte hitta skärminställningsfil för superuser. Innan du kör det här skriptet måste du först göra ändringar i skärminställningarna för superuser."
 
 if echo \$LANG | grep --quiet sv; then
     MSG_SUCCESS=\$MSG_SUCCESS_SV
@@ -48,14 +44,13 @@ else
 fi
 
 if [ -f $MONITOR_SETTINGS_FILE_SUPERUSER ]; then
-    sudo cp $MONITOR_SETTINGS_FILE_SUPERUSER $MONITOR_SETTINGS_FILE_SKELETON
-    echo "\$MSG_SUCCESS"
+    # Really the only command needing root access
+    cp $MONITOR_SETTINGS_FILE_SUPERUSER $MONITOR_SETTINGS_FILE_SKELETON
+    # Since this is now run as root we can't successfully call zenity directly...
+    runuser -u $SUPERUSER -- zenity --info --text "\$MSG_SUCCESS"
 else
-    echo "\$MSG_ERROR"
+    runuser -u $SUPERUSER -- zenity --info --text "\$MSG_ERROR"
 fi
-# Press enter to continue logic, so the user can see the echo message. Considered notify-send instead, but it cuts off the message.
-# Zenity could be an alternative, but we need sudo from the terminal anyway (unless we add the script to sudoers with no password), soooo.
-read _
 EOF
 
 cat << EOF > "$MONITOR_SCRIPT_DESKTOP_FILE"
@@ -64,11 +59,11 @@ Name=Copy monitor settings to user
 Name[da]=Kopier skærmindstillinger til Borger
 Name[sv]=Kopiera skärminställningar till Medborgar
 Type=Application
-Terminal=true
 Icon=preferences-desktop-display
-Exec=$MONITOR_SCRIPT
+Exec=sudo $MONITOR_SCRIPT
 EOF
 
+# Make the desktop shortcut launchable
 runuser -u $SUPERUSER dbus-launch gio set "$MONITOR_SCRIPT_DESKTOP_FILE" metadata::trusted true
 
 cat << EOF > $MONITOR_SUDOERS_SCRIPT
@@ -76,10 +71,10 @@ $SUPERUSER ALL = (root) NOPASSWD: $MONITOR_SCRIPT
 EOF
 
 chown superuser:superuser "$MONITOR_SCRIPT" "$MONITOR_SCRIPT_DESKTOP_FILE"
-chmod 500 $MONITOR_SCRIPT
+# Regarding the desktop file:
 # This is partially done to update the timestamp of the desktop file file so gio realizes its changed. We've tried touch for this, but sometimes, strangely,
 # that hasn't been enough
-chmod 500 "$MONITOR_SCRIPT_DESKTOP_FILE"
+chmod 500 $MONITOR_SCRIPT "$MONITOR_SCRIPT_DESKTOP_FILE"
 chmod 440 $MONITOR_SUDOERS_SCRIPT
 else
     rm --force "$MONITOR_SETTINGS_FILE_SKELETON" "$MONITOR_SCRIPT" "$MONITOR_SCRIPT_DESKTOP_FILE" "$MONITOR_SUDOERS_SCRIPT"
