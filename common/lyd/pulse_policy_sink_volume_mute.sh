a605d599fe4fcc64e4813f4edbf2bf8be160fb0b
Marcus Funch <mfm@magenta.dk>
2024-06-18 14:11:43 +0200

    Sound scripts: Rewrite volume script to also set mute/unmute

--- common/lyd/pulse_policy_sink_volume_mute.sh
+++ common/lyd/pulse_policy_sink_volume_mute.sh
@@ -4,16 +4,26 @@ set -ex
 
 ACTIVATE=$1
 SINK=$2
-MUTE_STATUS=$3
+PROPERTY_VALUE=$3
 
 OS2BORGERPC_PULSEAUDIO_CONFIG="/etc/pulse/default.pa.d/os2borgerpc.pa"
 OLD_OS2BORGERPC_PULSEAUDIO_CONFIG="/etc/pulse/profile.pa.d/os2borgerpc.pa"
-PROPERTY="set-sink-mute"
+PROPERTY_VOLUME="set-sink-volume"
+PROPERTY_MUTE="set-sink-mute"
+MAX_INT_VOLUME=65356
 
-if [ "$MUTE_STATUS" = "True" ]; then
-  PROPERTY_VALUE=1  # 1 mutes, 0 unmutes
+  # 1 mutes, 0 unmutes
+if [ "$PROPERTY_VALUE" = 0 ]; then
+  MUTE_MAYBE=1
 else
-  PROPERTY_VALUE=0
+  MUTE_MAYBE=0
+fi
+
+VOLUME=$(( MAX_INT_VOLUME * PROPERTY_VALUE / 100 ))
+
+if [ "$PROPERTY_VALUE" -lt 0 ] || [ "$PROPERTY_VALUE" -gt 100 ]; then
+  echo "Volume percentage must be between 0 and 100 inclusive. Exiting."
+  exit 1
 fi
 
 # 20.04 backwards compatibility - in 22.04 the default.pa.d directory and the reference to it is already there
@@ -44,8 +54,11 @@ create_os2borgerpc_pulseaudio_config_dir() {
 create_os2borgerpc_pulseaudio_config_dir
 
 # Current setting for this particular property for this particular card?: Delete it first
-sed --in-place "/$PROPERTY $SINK/d" $OS2BORGERPC_PULSEAUDIO_CONFIG
+sed --in-place --expression "/$PROPERTY_VOLUME $SINK/d" --expression "/$PROPERTY_MUTE $SINK/d" $OS2BORGERPC_PULSEAUDIO_CONFIG
 
-if [ "$ACTIVATE" = 'True' ]; then
-  echo "$PROPERTY $SINK $PROPERTY_VALUE" >> $OS2BORGERPC_PULSEAUDIO_CONFIG
+if [ "$ACTIVATE" = "True" ]; then
+	cat <<- EOF >> $OS2BORGERPC_PULSEAUDIO_CONFIG
+		$PROPERTY_VOLUME $SINK $VOLUME
+		$PROPERTY_MUTE $SINK $MUTE_MAYBE
+	EOF
 fi
